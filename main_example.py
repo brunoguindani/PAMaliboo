b98e44ba0e4cc809297cfe4315f641d53e1d3844
brunoguindani <brunjox96@hotmail.it>
2023-04-20 13:56:22 +0200

    Main file cleanup

--- main_example.py
+++ main_example.py
@@ -17,30 +17,29 @@ from pamaliboo.optimizer import Optimizer
 
 output_folder = 'outputs'
 database = 'gp_database.csv'
-init_history = 'resources/dummy_initial.csv'
-np.random.seed(42)
+init_history = os.path.join('resources', 'dummy_initial.csv')
+domain = os.path.join('resources', 'dummy_domain.csv')
+rng_seed = 42
+
+
+# Initialize and set relevant stuff
 debug = True if '-d' in sys.argv or '--debug' in sys.argv else False
-
-
-# Temporary code to take initialization values and remove temp files
-os.makedirs(output_folder, exist_ok=True)
-database_path = os.path.join(output_folder, database)
-
-
-# Set logging level
 logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+database_path = os.path.join(output_folder, database)
+np.random.seed(rng_seed)
+
 
 # Initialize library objects
+job_submitter = HyperqueueJobSubmitter(output_folder)
 constraints = {'result': (2, 6), 'result^2': (4, 36)}
 model = Ridge()
 acq = EIML(maximize_n_warmup=10, maximize_n_iter=100, constraints=constraints,
            models=[model, model])
-bounds = {'x1': (0, 5), 'x2': (0, 5)}
-kernel = Matern(nu=2.5)  # + WhiteKernel(noise_level_bounds=(0.2, 2))
+kernel = Matern(nu=2.5)
 gp = DGPR(database_path, feature_names=['f1', 'f2'], kernel=kernel)
-job_submitter = HyperqueueJobSubmitter(output_folder)
 # obj = DummyObjective()
-obj = DummyObjective(domain_file='resources/dummy_domain.csv')
-optimizer = Optimizer(acq, bounds, gp, job_submitter, obj, output_folder)
+obj = DummyObjective(domain_file=domain)
+opt_bounds = {'x1': (0, 5), 'x2': (0, 5)}
+optimizer = Optimizer(acq, opt_bounds, gp, job_submitter, obj, output_folder)
 optimizer.initialize(init_history)
 optimizer.maximize(n_iter=10, parallelism_level=2, timeout=3)
