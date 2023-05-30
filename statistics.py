0f3c7eeeb17cf382cbf4861468bd485ea2589b19
brunoguindani <bruno@guindani.org>
2023-05-30 11:16:28 +0200

    More statistics WIP

--- statistics.py
+++ statistics.py
@@ -26,8 +26,8 @@ opt_constraints = {'RMSD_0.75': (0, 2)}
 # Find real optimum
 df_truth = pd.read_csv(os.path.join('resources', 'ligen',
                                     'ligen_red_table.csv'))
-df_truth['obj'] = -df_truth['RMSD_0.75'] ** 3 * df_truth['TIME_TOTAL']
-df_truth.sort_values(by='obj', inplace=True, ascending=False)
+df_truth['target'] = -df_truth['RMSD_0.75'] ** 3 * df_truth['TIME_TOTAL']
+df_truth.sort_values(by='target', inplace=True, ascending=False)
 best = df_truth.iloc[0]
 
 # Loop over paralellism levels and RNG seeds
@@ -41,15 +41,36 @@ for par in parallelism_levels:
       rng_seeds.extend(other_seeds)
 
   for rng in rng_seeds:
+    print(f">>> par = {par}, rng = {rng}")
     # Get history dataframe for this experiment
     output_folder = os.path.join(root_output_folder, f'par_{par}',
                                                      f'rng_{rng}')
     hist = pd.read_csv(os.path.join(output_folder, 'history.csv'),
                        index_col='index')
-    noninit = (hist.index != -1)
-    res = pd.DataFrame(index=hist.index[noninit])
+    res = pd.DataFrame(index=hist.index)
 
     # Feasible observations with respect to the constraints
     res['feas'] = True
     for key, (lb, ub) in opt_constraints.items():
       res['feas'] = res['feas'] & (lb <= hist[key]) & (hist[key] <= ub)
+
+    # Feasible incumbents at each iteration
+    incumbents = []
+    curr_inc = None
+    for i in range(hist.shape[0]):
+      if res['feas'].iloc[i] and (curr_inc is None
+                                  or hist['target'].iloc[i] > curr_inc):
+        curr_inc = hist['target'].iloc[i]
+      incumbents.append(curr_inc)
+    res['incum'] = incumbents
+
+    # Simple relative regret
+    res['relreg'] = (res['incum'] - best['target']) / best['target']
+
+    # Print results and compute global metrics by removing initial points
+    noninit = (hist.index != -1)
+    res = res.loc[noninit]
+    print(res)
+    print("Unfeasible =", (~res['feas']).sum())
+    print("Average relative regret =", res['relreg'].mean())
+    print()
